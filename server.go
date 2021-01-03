8a35ccce7e049d80f54bb1411ab6f296a72fded3
muneer <muneer.puri.work@gmail.com>
2021-01-03 22:57:37 +0530

    infinite added

--- server.go
+++ server.go
@@ -14,7 +14,8 @@ const doc = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+	<meta charset="UTF-8">
+	<meta http-equiv="refresh" content="0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Test Table</title>
 	<style>
